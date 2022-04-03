5d34e9b541b11d349b7c9e8821d636d19fe3f126
Maor Lachish <89803221+mlachish@users.noreply.github.com>
2022-04-03 07:29:24 +0300

    Delete backend directory

--- back-end/index.ts
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port: Number | String = process.env.PORT || 4000
-
-app.listen(port, (): void => console.log('app listening on port ' + port))
